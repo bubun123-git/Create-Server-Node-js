fa1b664f0b09ce869f3018544b802124580d6f76
bubun123-git <bubundcool@gmail.com>
2024-02-16 09:43:02 +0530

    cleaned up code

---
+++ routes.js
@@ -0,0 +1,86 @@
+
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/home") {
+    fs.readFile("message.txt", "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<html>\n");
+        res.write("<head><title>Welcome Home</title></head>\n");
+        res.write(`<body>
+            <form action="/message" method="POST"><input type="text" name="msg">
+            <h1>${data}</h1>
+            <button type="submit">Submit</button></form>
+          </body>`);
+        res.write("</html>");
+        return res.end();
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<html>\n");
+        res.write("<head><title>Welcome Home</title></head>\n");
+        res.write(`<body>
+          <h1>${data}</h1>
+            <form action="/message" method="POST"><input type="text" name="msg">
+            <button type="submit">Submit</button></form>
+          </body>`);
+        res.write("</html>");
+        return res.end();
+      }
+    });
+  } else if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      let msg = Buffer.concat(body).toString();
+      msg = msg.split("=")[1];
+      fs.writeFile("message.txt", msg, (err) => {
+        if (err) {
+          console.error(err);
+          return res.end();
+        }
+        console.log(msg);
+        res.statusCode = 302;
+        res.setHeader("Location", "/home");
+        return res.end();
+      });
+    });
+  } else if (url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<html>\n");
+    res.write("<head><title>Welcome</title></head>\n");
+    res.write(`<body>
+        <h1>Welcome to About Us page</h1>
+      </body>`);
+    res.write("</html>");
+    return res.end();
+  } else if (url === "/node") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<html>\n");
+    res.write("<head><title>Welcome</title></head>\n");
+    res.write(`<body>
+        <h1>Welcome to my Node Js project</h1>
+      </body>`);
+    res.write("</html>");
+    return res.end();
+  } else {
+    console.log(req.url, req.method, req.headers);
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<html>\n");
+    res.write("<head><title>Hello World</title></head>\n");
+    res.write("<body><h1>Hello from my Node.js</h1></body>\n");
+    res.write("</html>\n");
+    return res.end();
+  }
+};
+
+module.exports = {
+  handler: requestHandler,
+  someText: 'Some hard coded text'
+};
